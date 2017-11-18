dec0176c8dbba628b15bc24299641d87c29a1881
pedrohsoares <pedrosoares1542@gmail.com>
2017-11-18 18:43:20 -0300

    Graphic Version Prolog

--- dados.pl
+++ dados.pl
@@ -1,5 +1,6 @@
 :- dynamic probabilidade/2.
 :- dynamic porcentagem/2.
+:- encoding(utf8).
 
 probabilidade(t1,0).
 probabilidade(t2,0).
@@ -14,39 +15,36 @@ entende(sim,sim).
 entende(yes,sim).
 entende(1,sim).
 
-doenca(t1,'Doenca Inflamatoria PElvica (DIP)').
+doenca(t1,'Doença Inflamatoria Pélvica (DIP)').
 doenca(t2,'Gonorreia').
 doenca(t3,'AIDS').
 doenca(t4,'Cancro mole').
-doenca(t5,'Sifilis').
+doenca(t5,'Sífilis').
 doenca(t6,'Herpes Genitais').
 
 perguntas(q1,'Voce tem tido corrimento vaginal e odor desagradavel?',[t1,t2]). /* DIP, Gonorreia - M */
 perguntas(q2,'Voce tem febre frequentemente',[t1,t2,t3,t4]). /* DIP, Gonorreia, AIDS, Cancro Mole */
-perguntas(q3,'Voce sente dores durante a relacao sexual',[t1,t4]). /* DIP, Cancro mole */
+perguntas(q3,'Voce sente dores durante a relação sexual',[t1,t4]). /* DIP, Cancro mole */
 perguntas(q4,'Voce tem tido dor ou ardencia ao urinar?',[t2,t6]). /* Gonorreia, Herpes */
-perguntas(q5,'Voce tem tido secrecao abundante de pus pela uretra',[t2]). /* Gonorreia - H */
+perguntas(q5,'Voce tem tido secreção abundante de pus pela uretra',[t2]). /* Gonorreia - H */
 perguntas(q6,'Voce tem tido sangramentos fora do periodo menstrual',[t2]). /* Gonorreia - M */
-perguntas(q7,'Dor ou inchaco em um dos testiculos',[t2]). /* Gonorreia - H */
+perguntas(q7,'Dor ou inchaço em um dos testiculos',[t2]). /* Gonorreia - H */
 perguntas(q8,'Voce tem tido nausea e vomitos',[t1]). /* DIP */
-perguntas(q9,'Voce tem perdido peso rapidamente ou notado uma diminuicao no apetite',[t3,t5]). /* AIDS, Sifilis */
+perguntas(q9,'Voce tem perdido peso rapidamente ou notado uma diminuição no apetite',[t3,t5]). /* AIDS, Sifilis */
 perguntas(q10,'Tosse seca prolongada e garganta arranhada',[t3]). /* AIDS */
 perguntas(q11,'Voce possui manchas avermelhadas ou pequenas bolinhas vermelhas na pele',[t3,t5,t6]). /* AIDS, Sifilis, Herpes */
-perguntas(q12,'Tem aparecido feridas dolorosas na regiao genital',[t4]). /* Cancro mole */
-perguntas(q13,'Voce apresenta inchaco ou dor na regiao da virilha',[t4]). /* Cancro mole */
+perguntas(q12,'Tem aparecido feridas dolorosas na região genital',[t4]). /* Cancro mole */
+perguntas(q13,'Voce apresenta inchaço ou dor na região da virilha',[t4]). /* Cancro mole */
 perguntas(q14,'Tem aparecido feridas indolores',[t5]). /* Sifilis */
 perguntas(q15,'Tem tido dores musculares',[t5,t6]). /* Sifilis, Herpes */
-perguntas(q16,'Ulceras na regiao dos genitais que podem sangrar e causar dor ao urinar',[t6]).  /* Herpes */
+perguntas(q16,'Úlceras na região dos genitais que podem sangrar e causar dor ao urinar',[t6]).  /* Herpes */
 
 increment([]).
-increment([H|T]):- probabilidade(H,Y), Y1 is Y+1, retract(probabilidade(H,_)), asserta(probabilidade(H,Y1)), increment(T).
+increment([H|T]):- probabilidade(H,Y), Y1 is Y+1, retract(probabilidade(H,_)), asserta(probabilidade(H,Y1)), Y1 \= 4, increment(T).
 
 init([]).
 init([H|T]):- asserta(probabilidade(H,0)), init(T). 
 
-gerarPorcentagens([],0).
-gerarPorcentagens([H|T],X):- probabilidade(H,Y), P is Y/4, P > 0, asserta(porcentagem(H,P)), gerarPorcentagens(T,X1), X is X1+P.
-gerarPorcentagens([_|T],X):- gerarPorcentagens(T,X).
-
-porcentagemGeral(H,G):-  porcentagem(H,Y), (Y =:= G -> R is Y*100 ; R is (Y*100) / G), doenca(H,X), write(X), write(' - '), write(R), write('%'),nl.
-porcentagemGeral([H|T],G) :- porcentagem(H,Y), (Y =:= G -> R is Y*100 ; R is (Y*100) / G), doenca(H,X), write(X), write(' - '), write(R), write('%'),nl, porcentagemGeral(T,G).
+gerarPorcentagens([],0,0).
+gerarPorcentagens([H|T],X,Q):- probabilidade(H,Y), P is Y/4, P > 0, asserta(porcentagem(H,P)), gerarPorcentagens(T,X1,Q1), X is X1+P, Q is Q1+1.
+gerarPorcentagens([_|T],X,Q):- gerarPorcentagens(T,X,Q).
--- main.pl
+++ main.pl
@@ -1,29 +1,96 @@
 :- include('dados.pl').
+:- use_module(library(pce)).
+:- use_module(library(pce_style_item)).
+:- encoding(utf8).
 
  /* busca a ser testada*/
-busca(t1) :- perguntar([q1,q6,q2,q3,q4,q8,q9,q10,q11,q12,q13,q14,q15,q16]), 
-	gerarPorcentagens([t1,t2,t3,t4,t5,t6],G), G > 0 , porcentagem(X,_), porcentagemGeral(X,G).
-busca(t1) :- write('Nao foi possivel identificar nenhuma doenca com as informacoes fornecidas!'),nl.
+busca(t1) :- perguntar([q1,q6,q2,q3,q4,q8,q9,q10,q11,q12,q13,q14,q15,q16]),
+	gerarPorcentagens([t1,t2,t3,t4,t5,t6],G,Q), Q > 0, resultado([t1,t2,t3,t4,t5,t6],G).
+busca(t1) :- gerarPorcentagens([t1,t2,t3,t4,t5,t6],G,Q), Q > 0, resultado([t1,t2,t3,t4,t5,t6],G).
+busca(t1) :- new(Di,dialog('Resultado')),
+			 new(L,label(texto,'Não foi possivel identificar nenhuma doença com as informações fornecidas!')),
+			 send(Di,append(L)),
+			 send(Di,open_centered), limpar.
+
 busca(t2) :- perguntar([q5,q7,q2,q3,q4,q8,q9,q10,q11,q12,q13,q14,q15,q16]),
-	gerarPorcentagens([t2,t3,t4,t5,t6],G), G > 0, porcentagem(X,_), porcentagemGeral(X,G).
-busca(t2) :- write('Nao foi possivel identificar nenhuma doenca com as informacoes fornecidas!'),nl.
+	gerarPorcentagens([t2,t3,t4,t5,t6],G,Q), Q > 0, resultado([t2,t3,t4,t5,t6],G).
+busca(t2) :- gerarPorcentagens([t2,t3,t4,t5,t6],G,Q), Q > 0, resultado([t2,t3,t4,t5,t6],G).
+busca(t2) :- new(Di,dialog('Resultado')),
+			 new(L,label(texto,'Não foi possivel identificar nenhuma doença com as informações fornecidas!')),
+			 send(Di,append(L)),
+			 send(Di,open_centered), limpar.
+
+resultado(X,G) :- new(Di,dialog('Resultado')),
+				  new(L3,label(texto,'As possiveis doenças são:')),
+				  send(Di, append(L3)),
+				  porcentagemGeral(X,G,50,Di),
+				  send(Di,open_centered),
+				  limpar.
+
+porcentagemGeral([],_,_,_).
+porcentagemGeral([H|T],G,Y,D):- porcentagem(H,Q), (Q == G -> R is Q*100 ; R is ((Q*100) / G)),
+								doenca(H,X),
+								new(Doenca,label(texto,X)),
+								new(Porcentagem,label(texto,R)),
+								new(T1,label(texto,' - ')),
+								new(T2,label(texto,'%')),
+								send(D,display,Doenca,point(20,Y)),
+								send(D,display,T1,point(220,Y)),
+								send(D,display,Porcentagem,point(280,Y)),
+								send(D,display,T2,point(350,Y)),
+								Y1 is Y+20, porcentagemGeral(T,G,Y1,D).
+porcentagemGeral([_|T],G,Y,D):- porcentagemGeral(T,G,Y,D).
+						          
+
 
 perguntar([]).
-perguntar([H|T]) :- perguntas(H,Q,S),
-        write(Q), write('? '),nl,
-         read(Resposta),nl,
-         (entende(Resposta,sim) -> increment(S), perguntar(T) ; perguntar(T)).
+perguntar([H|T]) :- perguntas(H,Q,S), new(Di,dialog('Diagnóstico médico')),
+				     new(L2,label(texto,'Responda a seguinte pergunta')),
+				     new(La,label(prob,Q)),
+				     new(B1,button(sim,and(message(Di,return,sim)))),
+				     new(B2,button(nao,and(message(Di,return,nao)))),
 
-verificar([]).
-verificar([H|T]) :- perguntar(H), verificar(T).
+				     send(Di,append(L2)),
+					 send(Di,append(La)),
+					 send(Di,append(B1)),
+					 send(Di,append(B2)),
 
-undo :- retract(probabilidade(_,_)), retract(porcentagem(_,_)),fail.
-undo :- init([t1,t2,t3,t4,t5,t6]).
+					 send(Di,default_button,sim),
+					 send(Di,open_centered),get(Di,confirm,Answer),
+					 send(Di,destroy),
+					 ((Answer==sim)-> increment(S), perguntar(T);
+					 perguntar(T)).
 
-inicio :- write('Voce e mulher?'),nl, read(Resposta), nl, 
-    (entende(Resposta,sim) 
-	-> busca(t1) ; busca(t2)
-    ).
+inicio :- new(Di,dialog('Diagnóstico médico')),
+     new(L,label(texto,'Responda a seguinte pergunta:')),
+     new(P,label(prob,'Você é mulher?')),
+     new(B1,button(sim,and(message(Di,return,sim)))),
+     new(B2,button(nao,and(message(Di,return,nao)))),
 
-identificar :- write('--*--Ola, vou te auxiliar a identificar se você possui alguma DST --*--')
-	   ,nl, nl, inicio, undo.
+     send(Di,append(L)),
+	 send(Di,append(P)),
+	 send(Di,append(B1)),
+	 send(Di,append(B2)),
+
+	 send(Di,default_button,sim),
+	 send(Di,open_centered),get(Di,confirm,Resposta),
+	 send(Di,destroy),
+	 ((Resposta==sim)-> busca(t1); busca(t2)).
+
+limpar :- retract(probabilidade(_,_)), fail.
+limpar :- retract(porcentagem(_,_)), fail.
+limpar :- init([t1,t2,t3,t4,t5,t6]), fail.
+limpar.
+
+iniciar:-
+	new(Menu, dialog('Sistema Especialista', size(1000,1000))),
+	new(L,label(text,'Sistema Identificador de Doenças Sexualmente Transmissiveis')),
+	new(@texto,label(text,'Responda a um breve questionário')),
+	new(@sair,button('Fechar',and(message(Menu, destroy),message(Menu,free)))),
+	new(@boton,button('Realizar Teste',message(@prolog,inicio))),
+
+	send(Menu,display,L,point(20,20)),
+	send(Menu,display,@boton,point(80,150)),
+	send(Menu,display,@texto,point(100,100)),
+	send(Menu,display,@sair,point(250,150)),
+	send(Menu,open_centered).
